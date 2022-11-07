db03263ba4f3b30fb6ffbc2af5a529eb7bee2922
Victor Alvarado <vito8916@gmail.com>
2022-11-06 20:47:54 -0600

    change url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://www.vicbox.tech/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		syntaxHighlight: 'prism',
